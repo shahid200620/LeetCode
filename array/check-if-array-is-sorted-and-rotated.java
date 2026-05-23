d53fe3cbe36a1a9d32f2de2c1a7c7a52cfa00767
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-05-23 16:47:31 +0530

    LC-check-if-array-is-sorted-and-rotated [Runtime: N/A; Memory: N/A]

---
+++ array/check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    public boolean check(int[] nums) {
+        int n = nums.length;
+
+        // Construct the rotated array
+        int[] checkSorted = new int[n];
+
+        // Iterate through all possible rotation offsets
+        for (int rotationOffset = 0; rotationOffset < n; ++rotationOffset) {
+            int currIndex = 0;
+            for (int index = rotationOffset; index < n; ++index) {
+                checkSorted[currIndex++] = nums[index];
+            }
+            for (int index = 0; index < rotationOffset; ++index) {
+                checkSorted[currIndex++] = nums[index];
+            }
+
+            // Check if the constructed array is sorted
+            boolean isSorted = true;
+            for (int index = 0; index < n - 1; ++index) {
+                if (checkSorted[index] > checkSorted[index + 1]) {
+                    isSorted = false;
+                    break;
+                }
+            }
+
+            // If sorted, return true
+            if (isSorted) {
+                return true;
+            }
+        }
+
+        // If no rotation makes the array sorted, return false
+        return false;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
