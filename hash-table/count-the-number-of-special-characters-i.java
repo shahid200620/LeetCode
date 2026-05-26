fd2de56802623e53feaa93d701fb925a154439f8
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-05-26 12:37:39 +0530

    LC-count-the-number-of-special-characters-i [Runtime: N/A; Memory: N/A]

---
+++ hash-table/count-the-number-of-special-characters-i.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public int numberOfSpecialChars(String word) {
+        Set<Character> s = new HashSet<>();
+        for (char c : word.toCharArray()) {
+            s.add(c);
+        }
+        int ans = 0;
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (s.contains(c) && s.contains((char) (c - 'a' + 'A'))) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
