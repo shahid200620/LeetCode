74a7145c96d657b080be76d28157952d6ca839ba
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-06-02 14:36:41 +0530

    LC-earliest-finish-time-for-land-and-water-rides-i [Runtime: N/A; Memory: N/A]

--- two-pointers/earliest-finish-time-for-land-and-water-rides-i.java
+++ two-pointers/earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,16 +1,31 @@
 class Solution {
-    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
-        int minTime = Integer.MAX_VALUE;
-        for ( int i=0; i< landStartTime.length; i++) {
-            for( int j=0; j< waterStartTime.length; j++){
-                int end1=Math.max(landStartTime[i],0)+landDuration[i];
-                int finish1=Math.max(waterStartTime[j],end1)+waterDuration[j];
-                minTime = Math.min(minTime, finish1);
-                int end2 = Math.max(waterStartTime[j],0)+waterDuration[j];
-                int finish2 = Math.max(landStartTime[i],end2)+landDuration[i];
-                minTime = Math.min(minTime, finish2);
+
+    public int earliestFinishTime(
+        int[] landStartTime,
+        int[] landDuration,
+        int[] waterStartTime,
+        int[] waterDuration
+    ) {
+        int n = landStartTime.length;
+        int m = waterStartTime.length;
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int land = landStartTime[i] + landDuration[i];
+                int land_water =
+                    Math.max(land, waterStartTime[j]) + waterDuration[j];
+                res = Math.min(res, land_water);
+
+                int water = waterStartTime[j] + waterDuration[j];
+                int water_land =
+                    Math.max(water, landStartTime[i]) + landDuration[i];
+                res = Math.min(res, water_land);
             }
         }
-        return minTime;
+        return res;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
