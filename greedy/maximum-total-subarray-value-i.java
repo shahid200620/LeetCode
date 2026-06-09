8d0e2201fc35130a5e905f91f61673186f39b032
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-06-09 20:09:12 +0530

    LC-maximum-total-subarray-value-i [Runtime: N/A; Memory: N/A]

---
+++ greedy/maximum-total-subarray-value-i.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public long maxTotalValue(int[] nums, int k) {
+        int m1 = Integer.MAX_VALUE;
+        int m2 = Integer.MIN_VALUE;
+        for (int x : nums) {
+            m1 = Math.min(m1, x);
+            m2 = Math.max(m2, x);
+        }
+        return (long) (m2 - m1) * k;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
