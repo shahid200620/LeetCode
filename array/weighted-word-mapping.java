60130f6bd63bc15939e71c44f373a3a9562aa1e2
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-06-13 21:42:47 +0530

    LC-weighted-word-mapping [Runtime: 2 ms; Memory: 46.4 MB]

---
+++ array/weighted-word-mapping.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder ans = new StringBuilder(words.length);
+        for (String word : words) {
+            int s = 0;
+            for (int i = 0; i < word.length(); i++) {
+                s += weights[word.charAt(i) - 'a'];
+            }
+            ans.append((char) ('z' - (s % 26)));
+        }
+        return ans.toString();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
