467d549a37d04e60c529722d5719d5391eede018
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-07-04 20:37:20 +0530

    LC-minimum-score-of-a-path-between-two-cities [Runtime: N/A; Memory: N/A]

---
+++ breadth-first-search/minimum-score-of-a-path-between-two-cities.java
@@ -0,0 +1,27 @@
+class Solution {
+    int find(int[] root, int i) {
+        if (root[i] == i)
+            return i;
+        return root[i] = find(root, root[i]);
+    }
+
+    public int minScore(int n, int[][] roads) {
+        int[] root = new int[n + 1];
+        for (int i = 0; i <= n; i++)
+            root[i] = i;
+
+        for (int[] r : roads)
+            root[find(root, r[0])] = find(root, r[1]);
+
+        int res = 10001;
+        for (int[] r : roads)
+            if (find(root, r[0]) == find(root, 1))
+                res = Math.min(res, r[2]);
+
+        return res;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
