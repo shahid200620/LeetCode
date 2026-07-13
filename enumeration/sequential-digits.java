84f997d28498a81d40c42e92bd461bfcd150ac01
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-07-13 08:14:21 +0530

    LC-sequential-digits [Runtime: N/A; Memory: N/A]

---
+++ enumeration/sequential-digits.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> ans = new ArrayList<>();
+
+        String s = "123456789";
+        String l = String.valueOf(low);
+        String h = String.valueOf(high);
+
+        for (int len = l.length(); len <= h.length(); len++) {
+            for (int start = 0; start <= 9 - len; start++) {
+                int num = Integer.parseInt(s.substring(start, start + len));
+                if (num >= low && num <= high) {
+                    ans.add(num);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
