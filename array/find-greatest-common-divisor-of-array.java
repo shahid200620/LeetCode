3cdc5605d1cf99849dfbdf862af0ded661d126d1
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-07-18 16:44:30 +0530

    LC-find-greatest-common-divisor-of-array [Runtime: N/A; Memory: N/A]

---
+++ array/find-greatest-common-divisor-of-array.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public int findGCD(int[] nums) {
+        int mx = Integer.MIN_VALUE;
+        int mn = Integer.MAX_VALUE;
+        for (int num : nums) {
+            mn = Math.min(mn, num);
+            mx = Math.max(mx, num);
+        }
+        return gcd(mx, mn);
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
