d339fd7eef930b18cf82e1fbf82ea3f2780628f6
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-07-24 18:48:43 +0530

    LC-number-of-unique-xor-triplets-ii [Runtime: N/A; Memory: N/A]

---
+++ math/number-of-unique-xor-triplets-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+        int m = 0;
+        for (int v : nums) {
+            m = Math.max(m, v);
+        }
+        int u = 1;
+        while (u <= m) {
+            u <<= 1;
+        }
+        boolean[] s = new boolean[u];
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                s[nums[i] ^ nums[j]] = true;
+            }
+        }
+        boolean[] t = new boolean[u];
+        for (int x = 0; x < u; x++) {
+            if (!s[x]) {
+                continue;
+            }
+            for (int v : nums) {
+                t[x ^ v] = true;
+            }
+        }
+        int ans = 0;
+        for (boolean b : t) {
+            if (b) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
