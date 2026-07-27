fad71e8af7c81d262c3bc62d79602554dbfd8d46
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-07-27 20:05:33 +0530

    LC-maximum-product-of-two-elements-in-an-array [Runtime: N/A; Memory: N/A]

---
+++ array/maximum-product-of-two-elements-in-an-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                ans = Math.max(ans, (nums[i] - 1) * (nums[j] - 1));
+            }
+        }
+        
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
