24e869a3805e0cb0a621f28b722fc1f004e6f3ae
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-03 14:56:52 +0530

    LC-longest-common-prefix [Runtime: N/A; Memory: N/A]

---
+++ string/longest-common-prefix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        strs=strs.sort()
+        s1=strs[0]
+        s2=strs[len(strs)-1]
+        s=""
+        for i in range(len(s1)):
+            if s1[i]==s2[i]:
+                s+=s1[i]
+            else :
+                return s
+        return s
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
