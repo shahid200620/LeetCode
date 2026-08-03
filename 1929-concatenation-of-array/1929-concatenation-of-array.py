0450d0f649009e8956515fa3eb3b9b43e1c5f665
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-03 11:38:20 +0530

    Time: 0 ms (100%), Space: 19.3 MB (40.91%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums+nums
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
