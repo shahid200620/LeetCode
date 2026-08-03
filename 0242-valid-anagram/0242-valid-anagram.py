c5185fef2c25ae0a31461e9ed367d08df81d2a19
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-03 12:00:51 +0530

    Time: 895 ms (6.2%), Space: 19.4 MB (75.57%) - LeetHub - Time: O(n^2), Space: O(1)

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        for i in s:
+            if i not in t or s.count(i) != t.count(i):
+                return False
+        else :
+            return True
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
