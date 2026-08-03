3144a0d93f4dc50dc0313397a8cddb39fec496ce
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-03 15:18:30 +0530

    LC-group-anagrams [Runtime: N/A; Memory: N/A]

---
+++ sorting/group-anagrams.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        s=defaultdict(list)
+        for i in strs:
+            a="".join(sorted(i))
+            s[a].append(i)
+        return list(s.values())
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
