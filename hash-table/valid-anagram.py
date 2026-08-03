55c3d6a43fa84d861b74c213f0c98680e724a154
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-03 12:00:41 +0530

    LC-valid-anagram [Runtime: 895 ms; Memory: 19.4 MB]

--- hash-table/valid-anagram.py
+++ hash-table/valid-anagram.py
@@ -1,7 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!=len(t):
-            retutn False
+            return False
         for i in s:
             if i not in t or s.count(i) != t.count(i):
                 return False
