df35ed85d07bc2b7076557f5473d9a6bc78cd88d
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-03 11:43:56 +0530

    LC-contains-duplicate [Runtime: N/A; Memory: N/A]

--- array/contains-duplicate.py
+++ array/contains-duplicate.py
@@ -1,7 +1,7 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         s=set(nums)
-        if len(nums) > len(s)):
+        if len(nums) > len(s):
             return True
         else:
             return False
