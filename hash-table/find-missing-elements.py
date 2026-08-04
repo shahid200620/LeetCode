e03d40adcb170b35fe73a3c34a12a05f7d8ecd3a
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-04 21:31:02 +0530

    LC-find-missing-elements [Runtime: 0 ms; Memory: 19.3 MB]

---
+++ hash-table/find-missing-elements.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        ans = []
+        for x, y in pairwise(nums):
+            ans.extend(range(x + 1, y))
+        return ans
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
