7e83691fc00e034e0bf2dfe75a728f1b450e128a
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-04 11:07:23 +0530

    LC-majority-element [Runtime: 0 ms; Memory: 21.2 MB]

--- array/majority-element.py
+++ array/majority-element.py
@@ -2,7 +2,7 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         nums.sort()
         mid=len(nums) //2
-        return nums(mid)
+        return nums[mid]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
