99c95f3d636dcff4a1de2e36db42e93053aa02f5
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-04 10:56:42 +0530

    LC-remove-element [Runtime: N/A; Memory: N/A]

--- array/remove-element.py
+++ array/remove-element.py
@@ -1,6 +1,11 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        return len(nums)-(nums.count(val))
+        k=0
+        for i in range(len(nums)):
+            if nums[i]!= val:
+                nums[0]=nums[i]
+                k+=1
+        return k
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
