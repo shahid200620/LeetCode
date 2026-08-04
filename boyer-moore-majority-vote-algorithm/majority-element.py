f2c3f487a09bbaed2eda641b9a3c76a01bcde7b2
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-04 11:05:46 +0530

    LC-majority-element [Runtime: N/A; Memory: N/A]

---
+++ boyer-moore-majority-vote-algorithm/majority-element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        mid=len(nums) //2
+        return nums(mid)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
