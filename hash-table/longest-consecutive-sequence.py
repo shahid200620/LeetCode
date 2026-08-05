16f1cf56b856c6b4e748acf7936bbc9b1f88ca17
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-05 14:26:38 +0530

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

---
+++ hash-table/longest-consecutive-sequence.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        n=list(set(nums))
+        n.sort()
+        tm=0
+        m=0
+        for i in range(1,len(n)):
+            if n[i-1]==n[i]-1:
+                m+=1
+            else :
+                tm=max(m,tm)
+                m=0
+        tm=max(m,tm)
+        return tm+1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
