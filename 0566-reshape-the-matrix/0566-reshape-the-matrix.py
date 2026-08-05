a4ca2215884e64a3f8bd450a89645ff8bc0505b7
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-05 10:35:02 +0530

    Time: 4 ms (10.63%), Space: 19.8 MB (91.43%) - LeetHub - Time: O(m*n), Space: O(r*c)

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -0,0 +1,18 @@
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        m=len(mat)
+        n=len(mat[0])
+        if r*c != m*n:
+            return mat
+        r=[[0]*c for i in range (r)]
+        cou=0
+        for i in range (m):
+            for j in range(n):
+                r[cou//c][cou%c]=mat[i][j]
+                cou+=1
+        return r
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
