1775dfca467ddb6697b70b2e7e2dad99bf399cbf
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-05 11:38:55 +0530

    LC-remove-duplicates-from-sorted-array [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n=set(nums)
+        return len(n)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
