dafe8f13cb14fef636591c99f6675802399fc92f
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-05 11:05:56 +0530

    LC-merge-sorted-array [Runtime: N/A; Memory: N/A]

---
+++ array/merge-sorted-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        n1=[nums1[i] for i in range (m)]
+        n2=[nums2[i] for i in range (n)]
+        n=n1+n2
+        n.sort()
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
