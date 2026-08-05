ef741dacf1ac804f2003d7149e9a2b685ad9dbc4
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-05 10:53:29 +0530

    Time: 10 ms (99.53%), Space: 25 MB (96.72%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,15 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        if 0 in nums:
+            l = [0] * len(nums)
+            if nums.count(0)==1:
+                i=nums.index(0)
+                nums.remove(0)
+                l[i]=math.prod(nums)
+            return l
+        a=math.prod(nums)
+        return [a//i for i in nums]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
