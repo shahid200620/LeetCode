5fc708b1160fb5897a41f97bfa1240e669e59a6a
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-05 12:12:52 +0530

    Time: 3 ms (79.94%), Space: 20.5 MB (75.38%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, n: List[int], t: int) -> List[int]:
+        d={}
+        for i in range(len(n)):
+            if t-n[i] in d:
+                return ([d[t-n[i]],i+1])
+            else:
+                d[n[i]]=i+1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
