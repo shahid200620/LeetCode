e5e9015d8b675c110bb1edcebb416fa61fcceca6
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-07 11:00:05 +0530

    LC-rotate-array [Runtime: 0 ms; Memory: 26.4 MB]

---
+++ two-pointers/rotate-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        c=len(nums)-k
+        nums[::]=nums[c:]+nums[:c]      
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
