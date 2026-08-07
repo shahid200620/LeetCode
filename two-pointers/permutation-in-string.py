c79fc6f4c23426f9c199ed2e5ae63b1ba76b2b1c
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-07 10:20:47 +0530

    LC-permutation-in-string [Runtime: N/A; Memory: N/A]

--- two-pointers/permutation-in-string.py
+++ two-pointers/permutation-in-string.py
@@ -2,14 +2,17 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         k=len(s1)
         s1=sorted(s1)
+        s4="".join(s1)
+        # print(s1)
         for i in range(len(s2)-k):
             l=[]
-            l.append(s2[i])
-            l.append(s2[i])
-            l.append(s2[i])
-            s="".join().l
+            for j in range(i,i+k):
+                l.append(s2[j])
+            s="".join(l)
             s=sorted(s)
-            if s == s1:
+            s3="".join(s)
+            # print(s)
+            if s3 == s4:
                 return True
         return False
 
