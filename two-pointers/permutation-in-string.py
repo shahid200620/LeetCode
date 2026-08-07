8402fc6a32d4175063e924780185b2fc36eab398
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-07 10:25:57 +0530

    LC-permutation-in-string [Runtime: N/A; Memory: N/A]

--- two-pointers/permutation-in-string.py
+++ two-pointers/permutation-in-string.py
@@ -3,19 +3,16 @@ class Solution:
         k=len(s1)
         s1=sorted(s1)
         s4="".join(s1)
-        # print(s1)
-        for i in range(len(s2)-k):
+        for i in range(len(s2)-k+1):
             l=[]
             for j in range(i,i+k):
                 l.append(s2[j])
             s="".join(l)
             s=sorted(s)
             s3="".join(s)
-            # print(s)
             if s3 == s4:
                 return True
         return False
-
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
