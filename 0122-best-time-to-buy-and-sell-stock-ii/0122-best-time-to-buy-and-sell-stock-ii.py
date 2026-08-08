ffef9054324dcd3ef6fef6c896bd2f270b8d63c0
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-08 11:58:11 +0530

    Time: 2 ms (67.52%), Space: 20.5 MB (21.53%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
+
+        return profit
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
