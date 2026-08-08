7c292e1c8787c5e43e36c5931b15605ffdcbab42
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-08 15:00:50 +0530

    LC-reverse-string [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/reverse-string.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reverseString(self, s):
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
