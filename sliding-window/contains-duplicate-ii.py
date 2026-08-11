72d4921d1bf96a725f8854a5dae1b1ca9559d703
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-11 11:43:46 +0530

    LC-contains-duplicate-ii [Runtime: N/A; Memory: N/A]

--- sliding-window/contains-duplicate-ii.py
+++ sliding-window/contains-duplicate-ii.py
@@ -1,13 +1,13 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-
-        for i in range(len(nums)):
-            for j in range (i,len(nums)):
-                if i!=j and nums[i] == nums[j] and abs(i - j) <= k:
-                    return True
+        n=len(nums)
+        if n==len(set(nums)):
+            return False
+        for i in range (n):
+            if nums[i] in nums[i+1:i+k+1]:
+                return True
         else :
             return False
-
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
