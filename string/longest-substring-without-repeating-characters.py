695a839f01158e666f16ac5a7da5dfc9d290a9ad
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-11 14:41:52 +0530

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

--- string/longest-substring-without-repeating-characters.py
+++ string/longest-substring-without-repeating-characters.py
@@ -1,18 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        n = len(s)
-        maxLength = 0
-        charSet = set()
-        left = 0
-        
-        for right in range(n):
-            if s[right] not in charSet:
-                charSet.add(s[right])
-                maxLength = max(maxLength, right - left + 1)
-            else:
-                while s[right] in charSet:
-                    charSet.remove(s[left])
-                    left += 1
-                charSet.add(s[right])
-        
-        return maxLength  
+        l = 0
+        a = set()
+        ans = 0
+
+        for r in range(len(s)):
+            while s[r] in a:
+                a.remove(s[l])
+                l += 1
+
+            a.add(s[r])
+            ans = max(ans, r - l + 1)
+
+        return ans
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
