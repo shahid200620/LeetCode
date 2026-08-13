da419e425c6d69d84ab80a1fe1ba766b4f527925
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-13 11:58:18 +0530

    LC-implement-stack-using-queues [Runtime: N/A; Memory: N/A]

---
+++ design/implement-stack-using-queues.py
@@ -0,0 +1,22 @@
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+    def pop(self) -> int:
+        return self.q.popleft()
+        
+    def top(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
