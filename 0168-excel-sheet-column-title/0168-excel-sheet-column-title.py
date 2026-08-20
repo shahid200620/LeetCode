389ab910bdfe38d83791435ec5dcaead5c045c9a
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-20 10:14:27 +0530

    Time: 0 ms (100%), Space: 19.3 MB (28.73%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
@@ -0,0 +1,13 @@
+class Solution:
+    def convertToTitle(self, c: int) -> str:
+        r=''
+        while c>0:
+            c-=1
+            rem=c%26
+            r=chr(ord("A")+rem)+r
+            c=c//26
+        return r
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
