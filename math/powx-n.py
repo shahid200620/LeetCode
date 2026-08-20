b2a7a5bbf3487115167879dba5e6289ecf7902f4
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-20 12:03:11 +0530

    LC-powx-n [Runtime: N/A; Memory: N/A]

---
+++ math/powx-n.py
@@ -0,0 +1,7 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x**n
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
