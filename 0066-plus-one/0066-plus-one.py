0c4e028539089f7988788988c6eb465530dca629
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-20 11:55:19 +0530

    Time: 0 ms (100%), Space: 19.2 MB (56.32%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,10 +1,13 @@
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        carry = 1
-        for i in range(len(digits) - 1, -1, -1):
-            s = digits[i] + carry
-            digits[i] = s % 10
-            carry = s // 10
-        if carry:
-            digits = [1] + digits
-        return digits
+    def plusOne(self, d: List[int]) -> List[int]:
+        n=len(d)
+        for i in range(n-1,-1,-1):
+            d[i]=d[i]+1
+            if d[i] < 10:
+                return d
+            d[i]=0
+        return [1]+d
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
