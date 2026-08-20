2c24f76802304f8b0e6aa67b31aec1e66c4eb33d
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-20 13:39:05 +0530

    LC-powx-n [Runtime: N/A; Memory: N/A]

--- math/powx-n.py
+++ math/powx-n.py
@@ -1,6 +1,26 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+
+        def calc_power(x, n):
+            if x == 0:
+                return 0
+            if n == 0:
+                return 1
+            
+            res = calc_power(x, n // 2)
+            res = res * res
+
+            if n % 2 == 1:
+                return res * x
+            
+            return res
+
+        ans = calc_power(x, abs(n))
+
+        if n >= 0:
+            return ans
+        
+        return 1 / ans 
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
