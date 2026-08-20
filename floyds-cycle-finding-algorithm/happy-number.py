0c36969a4c0030534b54b1e8a3903d316e71d4a2
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-08-20 11:45:14 +0530

    LC-happy-number [Runtime: N/A; Memory: N/A]

--- floyds-cycle-finding-algorithm/happy-number.py
+++ floyds-cycle-finding-algorithm/happy-number.py
@@ -1,20 +1,32 @@
-def co(n):
-        s=0
-        while (n>=10):
-            r=n%10
-            s+=(r*r)+s
-            n=n//10
-        return s+(n*n)
+# def co(n):
+#         s=0
+#         while (n>=10):
+#             r=n%10
+#             s+=(r*r)+s
+#             n=n//10
+#         return s+(n*n)
 class Solution:
     def isHappy(self, n: int) -> bool:
-        if n!=1 and n<9:
-            return False
-        while (n >=10):
-            n= co(n)
-        if n==1:
-            return True
-        else:
-            return False
+        # if n!=1 and n<9:
+        #     return False
+        # while (n >=10):
+        #     n= co(n)
+        # if n==1:
+        #     return True
+        # else:
+        #     return False
+        seen=set()
+        while n!=1:
+            if n in seen :
+                return False
+            seen.add(n)
+            s=0
+            while n!=0:
+                r=n%10
+                s+=(r*r)
+                n=n//10
+            n=s
+        return n==1
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
