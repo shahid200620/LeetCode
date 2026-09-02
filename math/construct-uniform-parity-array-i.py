c0ae74548b03f27f6eef5c416abd31742c169171
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-09-02 21:49:13 +0530

    LC-construct-uniform-parity-array-i [Runtime: N/A; Memory: N/A]

---
+++ math/construct-uniform-parity-array-i.py
@@ -0,0 +1,7 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
