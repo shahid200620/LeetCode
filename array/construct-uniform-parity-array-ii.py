fb06f0d76636e772cf5eabafc0e740661d0fafc5
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-09-03 21:02:26 +0530

    LC-construct-uniform-parity-array-ii [Runtime: N/A; Memory: N/A]

--- array/construct-uniform-parity-array-ii.py
+++ array/construct-uniform-parity-array-ii.py
@@ -9,7 +9,7 @@ class Solution:
                 hasOdd = True
         if mn & 1:
             return True
-        return not 
+        return not hasOdd
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
