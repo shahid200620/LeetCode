8d029edd50796e0fcff6b06a0af074c7b16091da
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-09-08 14:01:10 +0530

    LC-add-binary [Runtime: N/A; Memory: N/A]

--- simulation/add-binary.py
+++ simulation/add-binary.py
@@ -1,23 +1,22 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        class Solution:
-  def addBinary(self, a: str, b: str) -> str:
-    s = []
-    carry = 0
-    i = len(a) - 1
-    j = len(b) - 1
+        carry = 0
+        res = []
+        
+        idxA, idxB = len(a) - 1, len(b) - 1
+        
+        while idxA >= 0 or idxB >= 0 or carry == 1:
+            if idxA >= 0:
+                carry += int(a[idxA])
+                idxA -= 1            
+            if idxB >= 0:
+                carry += int(b[idxB])
+                idxB -= 1            
 
-    while i >= 0 or j >= 0 or carry:
-      if i >= 0:
-        carry += int(a[i])
-        i -= 1
-      if j >= 0:
-        carry += int(b[j])
-        j -= 1
-      s.append(str(carry % 2))
-      carry //= 2
-
-    return ''.join(reversed(s))
+            res.append(str(carry % 2))
+            carry = carry // 2
+            
+        return "".join(res[::-1])
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
