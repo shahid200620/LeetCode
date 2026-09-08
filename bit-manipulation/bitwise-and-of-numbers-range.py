8371b82c832eb59598ffc3e723c5c8a9bee4f43e
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-09-08 14:07:00 +0530

    LC-bitwise-and-of-numbers-range [Runtime: N/A; Memory: N/A]

---
+++ bit-manipulation/bitwise-and-of-numbers-range.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        cnt = 0
+        while left != right:
+            left >>= 1
+            right >>= 1
+            cnt += 1
+        return left << cnt
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
