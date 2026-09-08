0f251990df971148692f720492cd54ba965a3363
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-09-08 14:06:20 +0530

    LC-reverse-integer [Runtime: N/A; Memory: N/A]

---
+++ math/reverse-integer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        res = 0
+        if x < 0:
+            res = int(str(x)[1:][::-1]) * -1
+        else:
+            res = int(str(x)[::-1])
+        
+        if res > 2 ** 31 - 1 or res < -2 ** 31:
+            return 0
+        
+        return res
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
