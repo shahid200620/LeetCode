9eaaa1c2db87d6f794bb8129c4bca4083ef51468
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-09-08 14:05:04 +0530

    LC-sum-of-two-integers [Runtime: N/A; Memory: N/A]

---
+++ math/sum-of-two-integers.py
@@ -0,0 +1,16 @@
+class Solution {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int carry = (a & b) << 1;
+            a = a ^ b;
+            b = carry;
+        }
+        
+        
+        return a;
+    }
+}
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
