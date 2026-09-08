8a46c4d6fcb7fdd16331e83730871e06d8027eeb
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-09-08 13:57:48 +0530

    LC-multiply-strings [Runtime: N/A; Memory: N/A]

--- string/multiply-strings.py
+++ string/multiply-strings.py
@@ -1,6 +1,26 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1)*int(num2))
+        if num1 == "0" or num2 == "0":
+            return "0"
+
+        n1, n2 = len(num1), len(num2)
+        result = [0] * (n1 + n2)
+
+        for i in range(n1 - 1, -1, -1):
+            for j in range(n2 - 1, -1, -1):
+                mul = (ord(num1[i]) - ord('0')) * (ord(num2[j]) - ord('0'))
+                p1, p2 = i + j, i + j + 1
+                total = mul + result[p2]
+
+                result[p2] = total % 10
+                result[p1] += total // 10
+
+        prodstring = ""
+        for digit in result:
+            if not (prodstring == "" and digit == 0):
+                prodstring += str(digit)
+
+        return prodstring
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
