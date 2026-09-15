c39d8bd82d9195a737980b8a64adc7fd49a5f6c9
Mohammed Shahid Ali Khan <150007961+shahid200620@users.noreply.github.com>
2026-09-15 20:42:07 +0530

    LC-maximum-number-of-non-overlapping-palindrome-substrings [Runtime: N/A; Memory: N/A]

---
+++ string/maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxPalindromes(self, s: str, k: int) -> int:
+        n = len(s)
+        if k == 1: return n
+
+        res = i = 0
+
+        while i <= n - k:
+            for d in (k, k + 1):
+                if i + d <= n and s[i : i + d] == s[i : i + d][::-1]:
+                    res += 1
+                    i += d
+                    break
+            else:
+                i += 1
+
+        return res
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
